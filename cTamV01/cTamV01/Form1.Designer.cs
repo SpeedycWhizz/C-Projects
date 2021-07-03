a40d28c31f6dcefe56dfd4066522bc4044b9b095
SpeedycWhizz <53400210+SpeedycWhizz@users.noreply.github.com>
2021-07-03 13:40:11 +0100

    TamV1.0 Coded in C#

---
+++ cTamV01/cTamV01/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace cTamV01
+{
+    partial class TamV1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBxMyMessages = new System.Windows.Forms.ListBox();
+            this.BtnChoiceOne = new System.Windows.Forms.Button();
+            this.BtnChoiceTwo = new System.Windows.Forms.Button();
+            this.RTxTypedAMessages = new System.Windows.Forms.RichTextBox();
+            this.LblMessaageCount = new System.Windows.Forms.Label();
+            this.LblMessages = new System.Windows.Forms.Label();
+            this.LblCharacterCountRes = new System.Windows.Forms.Label();
+            this.LblCharacterCount = new System.Windows.Forms.Label();
+            this.LblVerNo = new System.Windows.Forms.Label();
+            this.LblHomeMadeSoftware = new System.Windows.Forms.Label();
+            this.PBxEditModeLED = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBxEditModeLED)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LBxMyMessages
+            // 
+            this.LBxMyMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBxMyMessages.FormattingEnabled = true;
+            this.LBxMyMessages.ItemHeight = 20;
+            this.LBxMyMessages.Location = new System.Drawing.Point(12, 31);
+            this.LBxMyMessages.Name = "LBxMyMessages";
+            this.LBxMyMessages.Size = new System.Drawing.Size(228, 104);
+            this.LBxMyMessages.TabIndex = 0;
+            this.LBxMyMessages.SelectedIndexChanged += new System.EventHandler(this.LBxMyMessages_SelectedIndexChanged);
+            this.LBxMyMessages.MouseEnter += new System.EventHandler(this.LBxMyMessages_MouseEnter);
+            this.LBxMyMessages.MouseLeave += new System.EventHandler(this.LBxMyMessages_MouseLeave);
+            // 
+            // BtnChoiceOne
+            // 
+            this.BtnChoiceOne.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnChoiceOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnChoiceOne.Location = new System.Drawing.Point(12, 179);
+            this.BtnChoiceOne.Name = "BtnChoiceOne";
+            this.BtnChoiceOne.Size = new System.Drawing.Size(103, 33);
+            this.BtnChoiceOne.TabIndex = 1;
+            this.BtnChoiceOne.Text = "Copy";
+            this.BtnChoiceOne.UseVisualStyleBackColor = true;
+            this.BtnChoiceOne.Click += new System.EventHandler(this.BtnChoiceOne_Click);
+            this.BtnChoiceOne.MouseLeave += new System.EventHandler(this.BtnChoiceOne_MouseLeave);
+            this.BtnChoiceOne.MouseHover += new System.EventHandler(this.BtnChoiceOne_MouseHover);
+            // 
+            // BtnChoiceTwo
+            // 
+            this.BtnChoiceTwo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnChoiceTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnChoiceTwo.Location = new System.Drawing.Point(137, 179);
+            this.BtnChoiceTwo.Name = "BtnChoiceTwo";
+            this.BtnChoiceTwo.Size = new System.Drawing.Size(103, 33);
+            this.BtnChoiceTwo.TabIndex = 2;
+            this.BtnChoiceTwo.Text = "Delete";
+            this.BtnChoiceTwo.UseVisualStyleBackColor = true;
+            this.BtnChoiceTwo.Click += new System.EventHandler(this.BtnChoiceTwo_Click);
+            this.BtnChoiceTwo.MouseLeave += new System.EventHandler(this.BtnChoiceTwo_MouseLeave);
+            this.BtnChoiceTwo.MouseHover += new System.EventHandler(this.BtnChoiceTwo_MouseHover);
+            // 
+            // RTxTypedAMessages
+            // 
+            this.RTxTypedAMessages.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RTxTypedAMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTxTypedAMessages.Location = new System.Drawing.Point(12, 249);
+            this.RTxTypedAMessages.Name = "RTxTypedAMessages";
+            this.RTxTypedAMessages.Size = new System.Drawing.Size(228, 128);
+            this.RTxTypedAMessages.TabIndex = 0;
+            this.RTxTypedAMessages.Text = "";
+            this.RTxTypedAMessages.TextChanged += new System.EventHandler(this.RTxTypedAMessages_TextChanged);
+            this.RTxTypedAMessages.KeyDown += new System.Windows.Forms.KeyEventHandler(this.RTxTypedAMessages_KeyDown);
+            this.RTxTypedAMessages.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RTxTypedAMessages_KeyUp);
+            // 
+            // LblMessaageCount
+            // 
+            this.LblMessaageCount.AutoSize = true;
+            this.LblMessaageCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMessaageCount.Location = new System.Drawing.Point(107, 145);
+            this.LblMessaageCount.Name = "LblMessaageCount";
+            this.LblMessaageCount.Size = new System.Drawing.Size(18, 20);
+            this.LblMessaageCount.TabIndex = 15;
+            this.LblMessaageCount.Text = "0";
+            // 
+            // LblMessages
+            // 
+            this.LblMessages.AutoSize = true;
+            this.LblMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMessages.Location = new System.Drawing.Point(16, 145);
+            this.LblMessages.Name = "LblMessages";
+            this.LblMessages.Size = new System.Drawing.Size(82, 20);
+            this.LblMessages.TabIndex = 14;
+            this.LblMessages.Text = "Messages";
+            // 
+            // LblCharacterCountRes
+            // 
+            this.LblCharacterCountRes.AutoSize = true;
+            this.LblCharacterCountRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCharacterCountRes.Location = new System.Drawing.Point(138, 381);
+            this.LblCharacterCountRes.Name = "LblCharacterCountRes";
+            this.LblCharacterCountRes.Size = new System.Drawing.Size(18, 20);
+            this.LblCharacterCountRes.TabIndex = 17;
+            this.LblCharacterCountRes.Text = "0";
+            // 
+            // LblCharacterCount
+            // 
+            this.LblCharacterCount.AutoSize = true;
+            this.LblCharacterCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCharacterCount.Location = new System.Drawing.Point(6, 381);
+            this.LblCharacterCount.Name = "LblCharacterCount";
+            this.LblCharacterCount.Size = new System.Drawing.Size(126, 20);
+            this.LblCharacterCount.TabIndex = 16;
+            this.LblCharacterCount.Text = "Character Count";
+            // 
+            // LblVerNo
+            // 
+            this.LblVerNo.AutoSize = true;
+            this.LblVerNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblVerNo.Location = new System.Drawing.Point(143, 403);
+            this.LblVerNo.Name = "LblVerNo";
+            this.LblVerNo.Size = new System.Drawing.Size(89, 20);
+            this.LblVerNo.TabIndex = 18;
+            this.LblVerNo.Text = "Version 1.0";
+            // 
+            // LblHomeMadeSoftware
+            // 
+            this.LblHomeMadeSoftware.AutoSize = true;
+            this.LblHomeMadeSoftware.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblHomeMadeSoftware.ForeColor = System.Drawing.Color.Gold;
+            this.LblHomeMadeSoftware.Location = new System.Drawing.Point(16, 438);
+            this.LblHomeMadeSoftware.Name = "LblHomeMadeSoftware";
+            this.LblHomeMadeSoftware.Size = new System.Drawing.Size(219, 13);
+            this.LblHomeMadeSoftware.TabIndex = 19;
+            this.LblHomeMadeSoftware.Text = "Home Made Software (c) 2021 - 2022";
+            this.LblHomeMadeSoftware.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PBxEditModeLED
+            // 
+            this.PBxEditModeLED.BackColor = System.Drawing.Color.Green;
+            this.PBxEditModeLED.Location = new System.Drawing.Point(186, 224);
+            this.PBxEditModeLED.Name = "PBxEditModeLED";
+            this.PBxEditModeLED.Size = new System.Drawing.Size(51, 10);
+            this.PBxEditModeLED.TabIndex = 20;
+            this.PBxEditModeLED.TabStop = false;
+            // 
+            // TamV1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(252, 465);
+            this.Controls.Add(this.PBxEditModeLED);
+            this.Controls.Add(this.LblHomeMadeSoftware);
+            this.Controls.Add(this.LblVerNo);
+            this.Controls.Add(this.LblCharacterCountRes);
+            this.Controls.Add(this.LblCharacterCount);
+            this.Controls.Add(this.LblMessaageCount);
+            this.Controls.Add(this.LblMessages);
+            this.Controls.Add(this.RTxTypedAMessages);
+            this.Controls.Add(this.BtnChoiceTwo);
+            this.Controls.Add(this.BtnChoiceOne);
+            this.Controls.Add(this.LBxMyMessages);
+            this.Name = "TamV1";
+            this.Text = "Tam V1 in C#";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TamV1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.PBxEditModeLED)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LBxMyMessages;
+        private System.Windows.Forms.Button BtnChoiceOne;
+        private System.Windows.Forms.Button BtnChoiceTwo;
+        private System.Windows.Forms.RichTextBox RTxTypedAMessages;
+   
+        internal System.Windows.Forms.Label LblMessaageCount;
+        internal System.Windows.Forms.Label LblMessages;
+        internal System.Windows.Forms.Label LblCharacterCountRes;
+        internal System.Windows.Forms.Label LblCharacterCount;
+        internal System.Windows.Forms.Label LblVerNo;
+        internal System.Windows.Forms.Label LblHomeMadeSoftware;
+        private System.Windows.Forms.PictureBox PBxEditModeLED;
+    }
+}
+
---
+++ cTamV01/cTamV01/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Reflection.Emit;
+using System.Drawing.Text;
+
+namespace cTamV01
+{
+    public partial class TamV1 : Form
+    {
+        ArrayList MyMessages = new ArrayList();
+        string SysDir = @"C:\TamV163";
+        string SysFilename = @"\Messages.txt";
+        bool EditMode;
+        
+
+        public TamV1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Runs Main Subrootines
+            SubLoadFile();
+            SubUpdateDisplay();
+            BtnChoiceOne.Enabled = false;
+            BtnChoiceTwo.Enabled = false;
+            PBxEditModeLED.Visible = false;
+
+        }
+
+       
+
+        private void LBxMyMessages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // LBx Stuff
+            // ---------
+
+            if (LBxMyMessages.SelectedItem != null)
+            {
+                
+                RTxTypedAMessages.Text = LBxMyMessages.SelectedItem.ToString();
+                BtnChoiceOne.Text = "Copy";
+                BtnChoiceTwo.Text = "Delete";
+            }
+            else 
+            {
+                BtnChoiceOne.Text = "Add";
+                BtnChoiceTwo.Text = "Clear";
+            }
+
+        }
+
+        private void RTxTypedAMessages_TextChanged(object sender, EventArgs e)
+        {
+            // Message Entry Area
+            //--------------------
+
+            // positition text cursor at end of message
+            // and allows me to edit the message
+
+            RTxTypedAMessages.SelectionStart = RTxTypedAMessages.TextLength + 1;
+            RTxTypedAMessages.Focus();
+
+            LblCharacterCountRes.Text = RTxTypedAMessages.TextLength.ToString();
+
+            if (RTxTypedAMessages.SelectionBackColor == Color.Yellow)
+            {
+                RTxTypedAMessages.SelectionBackColor = Color.White;
+                RTxTypedAMessages.Focus();
+            }
+
+            if (LBxMyMessages.SelectedItem !=null &&
+                RTxTypedAMessages.TextLength < LBxMyMessages.Text.Length ||
+                RTxTypedAMessages.TextLength > LBxMyMessages.Text.Length)
+            {
+                EditMode = true;
+                PBxEditModeLED.Visible = true;
+            }
+            else 
+            {
+                EditMode = false;
+                PBxEditModeLED.Visible = false;
+            }
+
+            if (EditMode && LBxMyMessages.SelectedItem==null &&
+                RTxTypedAMessages.TextLength>0)
+            {
+                BtnChoiceOne.Text = "Add";
+                BtnChoiceTwo.Text = "Delete";
+                PBxEditModeLED.Visible = false;
+            }
+
+            if (EditMode==true)
+            {
+                BtnChoiceOne.Text = "Edit";
+                BtnChoiceTwo.Text = "Copy";
+                BtnChoiceOne.Enabled = true;
+                BtnChoiceTwo.Enabled = true;
+                PBxEditModeLED.BackColor = Color.Green;
+            }
+
+            if (EditMode == false)
+            {
+                BtnChoiceOne.Text = "Copy";
+                BtnChoiceTwo.Text = "Delete";
+                BtnChoiceOne.Enabled = true;
+                BtnChoiceTwo.Enabled = true;
+                PBxEditModeLED.BackColor = Color.Green;
+            }
+
+            if (LBxMyMessages.SelectedItem == null && RTxTypedAMessages.TextLength == 0)
+            {
+                BtnChoiceOne.Text = "Add";
+                BtnChoiceTwo.Text = "Clear";
+                BtnChoiceOne.Enabled = false;
+                BtnChoiceTwo.Enabled = false;
+            }
+            else if (LBxMyMessages.SelectedItem == null && RTxTypedAMessages.TextLength > 0)
+            {
+                BtnChoiceOne.Text = "Add";
+                BtnChoiceTwo.Text = "Clear";
+                BtnChoiceOne.Enabled = true;
+                BtnChoiceTwo.Enabled = true;
+            }
+            else if (LBxMyMessages.SelectedItem != null && RTxTypedAMessages.TextLength == 0)
+            {
+                LBxMyMessages.ClearSelected();
+                PBxEditModeLED.Visible = false;
+                BtnChoiceOne.Text = "Add";
+                BtnChoiceTwo.Text = "Clear";
+                BtnChoiceOne.Enabled = true;
+                BtnChoiceTwo.Enabled = true;
+            }
+        }
+
+        private void BtnChoiceOne_Click(object sender, EventArgs e)
+        {
+            BtnOneActions();
+        }
+
+        private void BtnChoiceTwo_Click(object sender, EventArgs e)
+        {
+            BtnTwoActions();
+        }
+
+        private void LBxMyMessages_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void LBxMyMessages_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void BtnChoiceTwo_MouseHover(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+
+        }
+
+        private void BtnChoiceTwo_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void BtnChoiceOne_MouseHover(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void BtnChoiceOne_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void SubLoadFile()
+        {
+            // Reads Messages From Directory / File
+
+            int Counter = 0;
+            String ReadInMessages;
+
+            if (File.Exists(SysDir + SysFilename))
+            {
+                StreamReader sr = new StreamReader(SysDir + SysFilename);
+                while ((ReadInMessages = sr.ReadLine()) != null)
+                {
+                    MyMessages.Add(ReadInMessages);
+                    Counter++;
+                }
+                sr.Close();
+
+              
+            }
+            else
+            {
+                Directory.CreateDirectory(SysDir);
+                File.Create(SysDir + SysFilename).Close();
+            }
+
+
+
+            // if no message selected, then resets button
+            if (LBxMyMessages.SelectedItem == null)
+                {
+                    // displays button text
+                    BtnChoiceOne.Text = "Add";
+                    BtnChoiceTwo.Text = "Clear";
+                }
+            }
+            
+
+        
+
+        // Runs Subrootine
+        private void SubEnterMessage()
+        {
+            // Message Entry
+            MyMessages.Add(RTxTypedAMessages.Text);
+           // RTxTypedAMessages.Clear();
+        }
+    
+
+        private void SubSaveMessages()
+        {
+            // Saves All Messages
+            StreamWriter sw = new StreamWriter(SysDir + SysFilename);
+            foreach (String SavedMessages in MyMessages)
+            {
+                sw.WriteLine(SavedMessages);
+            }
+            sw.Close();
+        }
+
+        private void TamV1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SubSaveMessages();
+        }
+
+        private void SubCopyRoutine()
+        {
+            RTxTypedAMessages.SelectAll();
+            RTxTypedAMessages.SelectionBackColor = Color.Yellow;
+            RTxTypedAMessages.Copy();
+        }
+
+        private void SubDeleteRoutine()
+        {
+            MyMessages.RemoveAt(LBxMyMessages.SelectedIndex);
+        }
+             
+        private void BtnOneActions()
+        {
+            if (BtnChoiceOne.Text == "Add")
+            {
+                SubEnterMessage();
+                SubUpdateDisplay();
+            }
+
+            else if (BtnChoiceOne.Text == "Copy")
+            {
+                SubCopyRoutine();
+            }
+
+            else if (LBxMyMessages.SelectedItem != null  &&  EditMode)
+            { 
+                MyMessages.RemoveAt(LBxMyMessages.SelectedIndex);
+                SubEnterMessage();
+                SubUpdateDisplay();
+            }
+        }
+
+        private void BtnTwoActions()
+        {
+            if (BtnChoiceTwo.Text=="Copy")
+            {
+                SubCopyRoutine();
+            }
+            else if (BtnChoiceTwo.Text == "Delete")
+            {
+                SubDeleteRoutine();
+                SubUpdateDisplay();
+            }
+        }
+
+        private void RTxTypedAMessages_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+            
+        }
+
+        private void RTxTypedAMessages_KeyUp(object sender, KeyEventArgs e)
+        {
+               
+            
+        }
+
+        private void SubUpdateDisplay()
+        {
+            LBxMyMessages.Items.Clear();
+            foreach (String ReadInMessages in MyMessages)
+            {
+                LBxMyMessages.Items.Add(ReadInMessages);
+            }
+        }
+    }
+ 
+}
